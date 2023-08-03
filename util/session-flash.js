ecdca40f505c2e6b981c81c8249cfa8a0756b0a5
Christian Francis <cfrancis1989@gmail.com>
2023-08-03 01:31:06 -0400

    added heroku deployment

--- util/session-flash.js
+++ util/session-flash.js
@@ -1,24 +1,24 @@
 function getSessionData(req, defaultValues) {
   let sessionData = req.session.flashedData;
 
-    if (!sessionData) { 
-        sessionData = {
-            hasError: false,
-            ...defaultValues
-        };
-    }
-    
+  if (!sessionData) {
+    sessionData = {
+      hasError: false,
+      ...defaultValues,
+    };
+  }
+
   req.session.flashedData = null;
 
   return sessionData;
 }
 
 function flashDataToSession(req, data, action) {
-    req.session.flashedData = {
-        hasError: true,
-        ...data,
-    };
-    
+  req.session.flashedData = {
+    hasError: true,
+    ...data,
+  };
+
   req.session.save(action);
 }
 
