98e8053c83c56a8aa5cbc7a00139b5f8f1024e98
Christian Francis <cfrancis1989@gmail.com>
2023-08-04 00:36:26 -0400

    fixing heroku deployment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const mainRoutes = require("./routes/main.route");
 const adminRoutes = require("./routes/admin.route");
 
 // let port = 3000;
-let port = mongodb+srv://zazzys:zazzys73!@looking-for-job-app.tetsdac.mongodb.net/?retryWrites=true&w=majority;
+let port = mongodb+srv://zazzys:zazzys73!@looking-for-job-app.tetsdac.mongodb.net/?retryWrites=true&w=majority
 
 if (process.env.PORT) {
   port = process.env.PORT;
